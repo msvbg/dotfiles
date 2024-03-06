1d194539b2845ab4f25d8f7a85015fee5fb10ab1
Martin Svanberg <martin@martinsvanberg.com>
2024-03-06 18:23:43 +0100

    Add vscode wgsl support

--- modules/home-manager/vscode/extensions.nix
+++ modules/home-manager/vscode/extensions.nix
@@ -15,6 +15,12 @@ let
       version = "0.6.1";
       sha256 = "sha256-Jh4DmLEoCsA0lY1ad3jMQRhWXEoXmggAKW8Z9QGYJjY=";
     }
- ]);
+    {
+      name = "wgsl";
+      publisher = "polymeilex";
+      version = "0.1.17";
+      sha256 = "sha256-vGqvVrr3wNG6HOJxOnJEohdrzlBYspysTLQvWuP0QIw=";
+    }
+  ]);
 in
 marketplaceExtensions ++ packagedExtensions
