2814e6b6d8f3294d8002c70ee7ec581a09c414a6
Martin Svanberg <martin@martinsvanberg.com>
2024-04-01 16:41:07 +0200

    Change vs code theme

--- modules/home-manager/vscode/extensions.nix
+++ modules/home-manager/vscode/extensions.nix
@@ -22,6 +22,12 @@ let
       version = "0.1.17";
       sha256 = "sha256-vGqvVrr3wNG6HOJxOnJEohdrzlBYspysTLQvWuP0QIw=";
     }
+    {
+      name = "ayu";
+      publisher = "teabyii";
+      version = "1.0.5";
+      sha256 = "sha256-+IFqgWliKr+qjBLmQlzF44XNbN7Br5a119v9WAnZOu4=";
+    }
   ]);
 in
 marketplaceExtensions ++ packagedExtensions
