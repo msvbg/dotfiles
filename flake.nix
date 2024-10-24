78e35ef61f0e7c9be89fcc6c0756d15a8e1b3dc6
Martin Svanberg <martin@martinsvanberg.com>
2024-10-24 17:36:17 +0200

    bump rust

--- flake.nix
+++ flake.nix
@@ -8,7 +8,9 @@
     nix-darwin = { url = "github:LnL7/nix-darwin"; inputs.nixpkgs.follows = "nixpkgs"; };
     nix-index-database = { url = "github:Mic92/nix-index-database"; inputs.nixpkgs.follows = "nixpkgs"; };
     home-manager = { url = "github:nix-community/home-manager/release-24.05"; inputs.nixpkgs.follows = "nixpkgs"; };
-    fenix = { url = "github:nix-community/fenix/abc0549e3560189462a7d394cc9d50af4608d103"; inputs.nixpkgs.follows = "nixpkgs"; };
+
+    # rust nightly 2024-10-23
+    fenix = { url = "github:nix-community/fenix/2f52fbbb56bbf1208835677f0c8992f942a47791"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
